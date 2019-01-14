6671cfe9cd8500f764fdbeef24ed22756d289171
acrylic-style <rht0910@rht0910.tk>
2019-01-15 01:01:28 +0900

    need more points!

--- src/commands/points.js
+++ src/commands/points.js
@@ -6,6 +6,6 @@ module.exports = class extends Command {
   }
 
   async run(msg, settings, user, lang) {
-    msg.channel.send(f(lang.points, user.data.point.toLocaleString(), settings.data.point.toLocaleString(), Math.floor(Math.sqrt(4 + user.data.point/900)-1), Math.floor(Math.sqrt(4 + settings.data.point/1500)-1)))
+    msg.channel.send(f(lang.points, user.data.point.toLocaleString(), settings.data.point.toLocaleString(), Math.floor(Math.sqrt(4 + user.data.point/1000)-1), Math.floor(Math.sqrt(4 + settings.data.point/3000)-1)))
   }
 }
