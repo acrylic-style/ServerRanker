0c846046e6ff700eafa1b2cf2a04fe574093e4ab
acrylic-style <rht0910@rht0910.tk>
2019-01-18 20:12:46 +0900

    add eval command

---
+++ src/commands/eval.js
@@ -0,0 +1,32 @@
+const { commons: { f }, Logger, Command } = require('../server-ranker')
+const logger = Logger.getLogger('commands:eval', 'lightpurple')
+
+module.exports = class extends Command {
+  constructor() {
+    const opts = {
+      args: ['<Code>'],
+    }
+    super('eval', opts)
+  }
+
+  isAllowed(msg, owners) {
+    return owners.includes(msg.author.id)
+  }
+
+  async run(msg, settings, user, lang, args) {
+    if (msg.content.includes('token')) return msg.channel.send(lang.udonthaveperm)
+    if (!args[1]) return msg.channel.send(lang.invalid_args)
+    !(async () => {
+      if (args[1].includes('async:')) {
+        args[1] = args[1].replace(/async:/g, '')
+        return await eval(`(async () => {${args.slice(1).join(' ')}})()`)
+      } else return await eval(args.slice(1).join(' '))
+    })().then(data => {
+      logger.info(`Eval by ${msg.author.tag} (${msg.author.id}), Result: ${data}`)
+      msg.channel.send(`:ok_hand: (${data})`)
+    }).catch(e => {
+      logger.info(`Eval[failed] by ${msg.author.tag} (${msg.author.id}), Result: \`${e.message}\``)
+      msg.channel.send(f(lang.error, e.message))
+    })
+  }
+}
