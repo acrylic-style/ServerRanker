3adea6d17ec8c09fe47c97db5b5c2016ffb8132a
acrylic-style <me@acrylicstyle.xyz>
2019-04-08 14:40:27 +0900

    bug fix that can't use any commands in DM

--- src/structures/Command.js
+++ src/structures/Command.js
@@ -53,7 +53,7 @@ class Command {
    * @param {Discord.Message} msg
    */
   isAllowed(msg) {
-    return !msg.guild || msg.member.hasPermission(this.permission.bitfield)
+    return msg.member.hasPermission(this.permission.bitfield)
   }
 }
 
