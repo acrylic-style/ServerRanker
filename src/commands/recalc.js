346e999cae3f665f44b07c1618a31f3a91808985
acrylic-style <me@acrylicstyle.xyz>
2019-04-12 09:49:47 +0900

    fix recalc command and optimize

--- src/commands/recalc.js
+++ src/commands/recalc.js
@@ -2,7 +2,7 @@ const { Command } = require('../server-ranker')
 const {
   setIntervalAsync,
   clearIntervalAsync,
-} = require('set-interval-async/dynamic')
+} = require('set-interval-async/fixed')
 //const data = require('../data')
 let running
 let queue = []
@@ -15,6 +15,7 @@ module.exports = class extends Command {
 
   async run(msg) {
     const callback = async () => {
+      const repeat = async (callback, count) => { for (let i = 0;i <= count; i++ ) { await callback() } }
       const asyncForEach = async (array, callback) => {
         const ids = array.map(c => c.id)
         const size = array.size-1
@@ -26,36 +27,37 @@ module.exports = class extends Command {
       lastrun = Date.now()
       running = msg.guild.id
       msg.channel.send('Fetching all messages. It may up to 28 hours.\n:warning: This is an ALPHA feature.\nBugs can happen often(Also queue system is may not work)!')
-      let messages = []
+      let messages = 0
       let lastmsg = { id: msg.id }
       const min = 100
       const max = 300
+      const finished = []
       let points = 0
       let maxpoints = 0
-      let finished = false
       const f = setInterval(() => {
-        if (!finished) return
-        msg.channel.send(`Collected ${messages.length} messages.\nExpected random points: ${points} (Max points: ${maxpoints})`)
+        if (finished.filter(f => !f).length) return
+        msg.channel.send(`Collected ${messages} messages.\nExpected random points: ${points} (Max points: ${maxpoints})`)
         running = null
         clearInterval(f)
       }, 1000 * 10)
       await asyncForEach(msg.guild.channels.filter(c => c.type === 'text').filter(c => c.memberPermissions(msg.guild.me).has(1024)), async (c, i, a) => {
+        finished[i] = false
         const interval = await setIntervalAsync(async () => {
           const fetchedMessages = await c.fetchMessages({ limit: 100, before: lastmsg.id })
           lastmsg = fetchedMessages.last() || msg.id
-          messages = [...messages, ...fetchedMessages.filter(m => !m.author.bot)]
-          if (fetchedMessages.size <= 99 || messages.length >= 1000000) {
-            messages.forEach(() => {
+          messages = messages + fetchedMessages.filter(m => !m.author.bot).size
+          if (fetchedMessages.size <= 99 || messages >= 1000000) {
+            repeat(() => {
               points = points + Math.round((Math.floor(Math.random() * (max + 1 - min)) + min) * 0.95)
               maxpoints = maxpoints + (300 * 0.95)
-            })
-            if (messages.length >= 1000000) {
-              msg.channel.send(`:warning: You've reached fetch limit.\nCollected ${messages.length} messages.\nExpected random points: ${points} (Max points: ${maxpoints})`)
+            }, messages)
+            if (messages >= 1000000) {
+              msg.channel.send(`:warning: You've reached fetch limit.\nCollected ${messages} messages.\nExpected random points: ${points} (Max points: ${maxpoints})`)
               running = null
             }
             clearInterval(f)
             clearIntervalAsync(interval)
-            if (i >= a.size) finished = true
+            if (i >= a.size) finished[i] = true
           }
         }, 1000 * 10)
       })
@@ -65,15 +67,13 @@ module.exports = class extends Command {
       if (queue.includes(msg.guild.id)) return msg.channel.send('This server is already in queue!')
       queue.push(msg.guild.id)
       const queueInterval = setInterval(async () => {
-        queue.forEach((guildId, index) => {
-          if (!running && guildId === msg.guild.id && index < 1) {
-            clearInterval(queueInterval)
-            queue = queue.slice(1)
-            msg.channel.send('Starting in 5 minutes')
-            running = msg.guild.id
-            setTimeout(() => { callback() }, 300000)
-          }
-        })
+        if (!running && queue[0] === msg.guild.id) {
+          clearInterval(queueInterval)
+          queue = queue.slice(1)
+          msg.channel.send('Starting in 5 minutes')
+          running = msg.guild.id
+          setTimeout(() => { callback() }, 300000)
+        }
       }, 1000 * 5)
       msg.channel.send('Waiting for queue...')
       return
