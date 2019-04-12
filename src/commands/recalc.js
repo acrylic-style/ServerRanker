1bd2044f407e509713af7addab1031f1c9b83fc9
yuta0801 <yuta20030801@icloud.com>
2019-04-12 17:36:35 +0900

    fix bug

--- src/commands/recalc.js
+++ src/commands/recalc.js
@@ -34,26 +34,27 @@ module.exports = class extends Command {
   async execute(msg) {
     msg.channel.send('Fetching all messages. It may up to 28 hours.\n:warning: This is an ALPHA feature.\nBugs can happen often(Also queue system is may not work)!')
     const channels = msg.guild.channels.filter(c => c.type === 'text' && c.memberPermissions(msg.guild.me).has(1024))
-    const messages = await this.series(Array.from(channels))
+    const messages = await this.series(Array.from(channels.values()))
     const maxpoints = messages * 300 * 0.95
-    const points = Array.from({ length: messages }, () => Math.round(genpoint() * 0.95)).reduce((p, c) => p + c)
+    const points = Array.from({ length: messages }, () => Math.round(genpoint() * 0.95)).reduce((p, c) => p + c, 0)
     const warning = messages >= 1000000 ? ':warning: You\'ve reached fetch limit.\n' : ''
     msg.channel.send(warning + `Collected ${messages} messages.\nExpected random points: ${points} (Max points: ${maxpoints})`)
     this.running = null
   }
 
   async series(channels, before, total = 0) {
+    if (!channels.length) return total
     const channel = channels.shift()
     const sum = total + await this.fetch(channel, before)
     if (sum >= 1000000) return sum
-    return await this.series(channels)
+    return await this.series(channels, before, sum)
   }
 
   fetch(channel, before, size = 0) {
     return new Promise(async resolve => {
       const messages = await channel.fetchMessages({ limit: 100, before })
       const sum = size + messages.filter(m => !m.author.bot).size
-      if (messages.size <= 99 || sum >= 1000000) return resolve(sum)
+      if (messages.size <= 99 || sum >= 200) return resolve(sum)
       setTimeout(() => resolve(this.fetch(channel, messages.last().id, sum)), 10e3)
     })
   }
