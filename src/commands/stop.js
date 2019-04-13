1175d7c681008d437027a21a3ff30f6cae24f494
acrylic-style <me@acrylicstyle.xyz>
2019-04-14 01:29:24 +0900

    await?

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -6,7 +6,7 @@ module.exports = class extends Command {
   }
 
   async run(msg) {
-    msg.channel.send(':wave:')
+    await msg.channel.send(':wave:')
     process.kill(process.pid, 'SIGKILL')
   }
 }
