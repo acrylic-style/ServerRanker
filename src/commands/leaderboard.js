6f629970a2db667753365004259d0f4f48d83da3
acrylic-style <me@acrylicstyle.xyz>
2019-04-16 14:29:19 +0900

    pp (not usable, leaderboard only)

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -27,21 +27,32 @@ module.exports = class extends Command {
       embed.setFooter('https://server-ranker.ga/leaderboard/server')
       msg.channel.send(embed)
     } else {
-      const users = await data.getUserLeaderboard()
-      const getUser = id => {
-        return msg.client.users.has(id) ? msg.client.users.get(id).username : 'Unknown User'
+      let users
+      let str = 'points'
+      let property = 'point'
+      let div = 1000
+      let langproperty = 'points'
+      if (args.includes('pp')) {
+        users = await data.getppUserLeaderboard()
+        str = 'pp'
+        property = 'pp'
+        langproperty = 'pp'
+        div = 1
+      } else {
+        users = await data.getUserLeaderboard()
       }
+      const getUser = async id => (await msg.client.fetchUser(id)).tag
       const embed = new Discord.RichEmbed()
         .setTimestamp()
         .setColor([0,255,0])
         .setTitle('Leaderboard')
-        .setDescription(f(lang.points, user.point.toLocaleString(), server.point.toLocaleString(), Math.floor(Math.sqrt(4 + user.point/1000)-1), Math.floor(Math.sqrt(4 + server.point/3000)-1)))
+        .setDescription(f(lang[langproperty], user[property].toLocaleString(), Math.floor(Math.sqrt(4 + user[property]/div)-1)))
         .setFooter(`Want to see server leaderboard? Type \`${server.prefix || 'sr!'}leaderboard server\``)
-      if (users[0]) embed.addField(':first_place:', `${parseInt(users[0].point).toLocaleString()} points (${getUser(users[0].user_id)})`)
-      if (users[1]) embed.addField(':second_place:', `${parseInt(users[1].point).toLocaleString()} points (${getUser(users[1].user_id)})`)
-      if (users[2]) embed.addField(':third_place:', `${parseInt(users[2].point).toLocaleString()} points (${getUser(users[2].user_id)})`)
-      if (users[3]) embed.addField('<:fourth_place:534409887027953694>', `${parseInt(users[3].point).toLocaleString()} points (${getUser(users[3].user_id)})`)
-      if (users[4]) embed.addField('<:fifth_place:534410165169029120>', `${parseInt(users[4].point).toLocaleString()} points (${getUser(users[4].user_id)})`)
+      if (users[0]) embed.addField(':first_place:', `${parseInt(users[0][property]).toLocaleString()} ${str} (${await getUser(users[0].user_id)})`)
+      if (users[1]) embed.addField(':second_place:', `${parseInt(users[1][property]).toLocaleString()} ${str} (${await getUser(users[1].user_id)})`)
+      if (users[2]) embed.addField(':third_place:', `${parseInt(users[2][property]).toLocaleString()} ${str} (${await getUser(users[2].user_id)})`)
+      if (users[3]) embed.addField('<:fourth_place:534409887027953694>', `${parseInt(users[3][property]).toLocaleString()} ${str} (${await getUser(users[3].user_id)})`)
+      if (users[4]) embed.addField('<:fifth_place:534410165169029120>', `${parseInt(users[4][property]).toLocaleString()} ${str} (${await getUser(users[4].user_id)})`)
       msg.channel.send(embed)
     }
   }
