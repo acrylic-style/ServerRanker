bc0faa3e91b6deb02d3ff2f18c21408850f9093c
acrylic-style <me@acrylicstyle.xyz>
2019-04-21 05:14:19 +0900

    use variable

--- src/functions/genpp.js
+++ src/functions/genpp.js
@@ -1,10 +1,4 @@
-module.exports = msg => 
-  Math.round(
-    Math.min(
-      300 * (
-        msg.content.length / 750 - (
-          /(\w)\1{10,}/g.test(msg.content) ? (msg.content.length / 750 / 4) : 0
-        )
-      ), 200
-    ) * 100
-  ) / 100
+module.exports = msg => {
+  const divby750 = msg.content.length / 750
+  return Math.round(Math.min(300 * (divby750 - (/(\w)\1{10,}/g.test(msg.content) ? (divby750 / 4) : 0)), 200) * 100) / 100
+}
