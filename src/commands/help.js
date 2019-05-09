700be48c4871a2dca7775fcc6e7bfbdd3b765289
acrylic-style <me@acrylicstyle.xyz>
2019-05-10 04:18:50 +0900

    a tiny fix

--- src/commands/help.js
+++ src/commands/help.js
@@ -10,6 +10,10 @@ module.exports = class extends Command {
       const { commands } = require(__dirname + '/../commands')
       const command = commands[args[1]]
       if (!command) return msg.channel.send(f(lang.no_command, args[1]))
+      const callback = p => {
+        const nounderbar = p.replace(/([A-Z].*?)(_(.*?))/g, '$1 ')
+        return nounderbar.replace(/\b[A-Z]{2,}\b/g, str => str.toLowerCase())
+      }
       const embed = new Discord.RichEmbed()
         .setTitle('About this command')
         .setDescription(
@@ -17,7 +21,7 @@ module.exports = class extends Command {
           + `\n\nUsage: ${(await data.getServer(msg.guild.id)).prefix}${args[1]} ${command.args !== [] ? command.args.join('\n') : ''}`
           + `\nAlias: ${command.alias !== [] ? command.alias.join('\n') : lang.no}`
           + `\nAllowed in: ${command.allowedIn.join(', ')}`
-          + `\nRequired permissions for you: ${command.permission.ALL ? command.permission.toArray().join(', ') : 'None'}`
+          + `\nRequired permissions for you: ${command.permission.bitfield ? command.permission.toArray(false).map(callback).join(', ') : 'None'}`
           + `\nIs special command: ${command.requiredOwner ? lang.yes : lang.no}`
           + `\nIs enabled: ${command.enabled ? lang.yes : lang.no}`)
         .setTimestamp()
