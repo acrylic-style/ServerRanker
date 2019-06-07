3e5afbc02d689d4234c2fa1fd88be9da6c0ad2af
acrylic-style <me@acrylicstyle.xyz>
2019-06-07 18:03:47 +0900

    show battlepass status

--- commands/battlepass.js
+++ commands/battlepass.js
@@ -11,7 +11,7 @@ module.exports = class extends Command {
   async run(msg, lang, args, sendDeletable) {
     const user = await data.getUser(msg.author.id)
     const embed = new Discord.RichEmbed()
-      .setTitle(f(lang['battlepass'], config.battlepass.currentSeason) + ` - ${lang['tier']} ${user.bp_tier || Math.floor(Math.sqrt(4 + user.exp/2)-1)}`)
+      .setTitle(f(lang['battlepass'], config.battlepass.currentSeason) + `${msg.author.premium ? ' (Premium)' : ' (FreePass, buy Discord Nitro for upgrade!... not yet.)'} - ${lang['tier']} ${user.bp_tier || Math.floor(Math.sqrt(4 + user.exp/2)-1)}`)
       .setDescription(f(lang['seasonEndsAt'], moment(config.battlepass.seasonEndsAt).locale(user.language).fromNow(true)))
       .setColor([0,255,0])
     sendDeletable(embed)
