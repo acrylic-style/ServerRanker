9f6d0aa3dacb37ad91885ecff5c1d84d1f007c58
acrylic-style <me@acrylicstyle.xyz>
2019-10-03 09:20:10 +0900

    remove unused file

--- src/functions/genpp.js
+++
@@ -1,4 +0,0 @@
-module.exports = msg => {
-  const divby750 = msg.content.length / 750
-  return Math.round(Math.min(300 * (divby750 - (/(\w)\1{10,}/g.test(msg.content) ? (divby750 / 4) : 0)), 200) * 100) / 100
-}
