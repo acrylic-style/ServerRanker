2cca45a632416a0b76b131680d81f970a24cd0a4
acrylic-style <me@acrylicstyle.xyz>
2019-10-03 09:53:19 +0900

    use asyncforeach

--- migrations/recalcAllTiersAndExp.js
+++ migrations/recalcAllTiersAndExp.js
@@ -3,11 +3,16 @@
   const logger = LoggerFactory.getLogger('migrations:recalcAllTiersAndExp')
   const data = require('../src/data')
   const getTier = require('../src/functions/getTier')
+  const asyncForEach = async (array, callback) => {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array)
+    }
+  }
   logger.info('first, we\'ll load all users.')
   const users = await data.getAllUsers()
-  users.forEach(async user => {
+  asyncForEach(users, async user => {
     logger.info('processing user: ' + user.user_id)
-    data.User.update({ exp: await this.calcWeightedExp(user.user_id) }, {
+    await data.User.update({ exp: await data.calcWeightedExp(user.user_id) }, {
       where: { user_id: user.user_id },
     })
     await data.addUserBattlePassTier(user.user_id, getTier((await data.getUser(user.user_id)).exp))
