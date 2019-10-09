a0b25aacb1510addc325c1cdc21779646d81539a
acrylic-style <me@acrylicstyle.xyz>
2019-10-09 20:01:25 +0900

    oof

--- src/functions/addpoint.js
+++ src/functions/addpoint.js
@@ -5,7 +5,7 @@ module.exports = async msg => {
   if (!msg.guild) return
   const random = generate()
   const multiplierCount = await data.activatedMultipliers(msg.guild.id)
-  const times = (await data.getPersonalPointBoost(msg.author.id)/100) + multiplierCount + 1
+  const times = ((await data.getPersonalPointBoost(msg.author.id))/100) + multiplierCount + 1
   await data.addUserPoint(msg.author.id, random * times)
   await data.addServerPoint(msg.guild.id, random * times)
 }
