919dd0d3d6181cd5ce826aea2113ffcd714eb6bf
acrylic-style <me@acrylicstyle.xyz>
2019-10-09 22:15:06 +0900

    language is now configurable

--- commands/settings.js
+++ commands/settings.js
@@ -6,7 +6,8 @@ module.exports = class extends Command {
   constructor() {
     const opts = {
       args: [
-        'config rewardsNotifications <true/false>',
+        'config rewardsNotifications [true/false]',
+        'config language [ja/en]',
       ],
       alias: ['config', 'setting'],
     }
@@ -19,6 +20,10 @@ module.exports = class extends Command {
       if (args[2] === 'rewardsNotifications') {
         if (args[3] !== 'true' && args[3] !== 'false') return await sendDeletable(`Settings ${args[2]} is \`${(await data.getUser(msg.author.id)).configRewardsNotifications}\`.`)
         await data.updateUser(msg.author.id, 'configRewardsNotifications', args[3] === 'true' ? true : false)
+        return await msg.channel.send(`Updated ${args[2]} to: \`${args[3]}\``)
+      } else if (args[2] === 'language') {
+        if (!['en', 'ja'].includes(args[3])) return await sendDeletable(`Settings ${args[2]} is \`${(await data.getUser(msg.author.id)).language}\`.`)
+        await data.updateUser(msg.author.id, 'language', args[3])
         return await msg.channel.send(`Updated ${args[2]} to: \`${args[3]}\``)
       } else return await sendDeletable(lang.invalid_args)
     } else return await sendDeletable(lang.invalid_args)
