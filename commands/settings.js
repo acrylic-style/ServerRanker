9a41bb9c22bef2af885c7098d48dc64bf8c9110d
acrylic-style <me@acrylicstyle.xyz>
2019-10-09 22:09:49 +0900

    it was non existent column

--- commands/settings.js
+++ commands/settings.js
@@ -18,7 +18,7 @@ module.exports = class extends Command {
     if (args[1] === 'config') {
       if (args[2] === 'rewardsNotifications') {
         if (args[3] !== 'true' && args[3] !== 'false') return await sendDeletable(`Settings ${args[2]} is \`${(await data.getUser(msg.author.id)).configRewardsNotifications}\`.`)
-        data.updateUser(msg.author.id, args[2], args[3] === 'true' ? true : false)
+        await data.updateUser(msg.author.id, 'configRewardsNotifications', args[3] === 'true' ? true : false)
         return await msg.channel.send(`Updated ${args[2]} to: \`${args[3]}\``)
       } else return await sendDeletable(lang.invalid_args)
     } else return await sendDeletable(lang.invalid_args)
